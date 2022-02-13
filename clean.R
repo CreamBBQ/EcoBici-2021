77798a24b3c807a85d5e37083440060f8f0a64cd
CreamBBQ <jgmd1306@gmail.com>
2022-02-13 07:52:30 -0300

    Primer gráfico: densidad de estaciones por barrios en la ciudad

--- clean.R
+++ clean.R
@@ -1,11 +1,13 @@
 rm(list = ls())
-setwd("/home/creambbq/code/bikes")
+setwd("/home/creambbq/code/bikes/archivos")
 
-#Load data
+##load data
 ecoBiciData <- read.table("trips_2021.csv", header = TRUE, sep = ",", stringsAsFactors = TRUE)
+
+##clean
 ecoBiciData$id_recorrido <- NULL
 ecoBiciData$id_estacion_origen <- NULL
 ecoBiciData$id_estacion_destino <- NULL
 ecoBiciData$direccion_estacion_origen <- NULL
 ecoBiciData$direccion_estacion_destino <- NULL
-#save(ecoBiciData, file = "cleanData.Rdata")
+#save(ecoBiciData, file = "cleanData.RData")
---
+++ test.R
@@ -0,0 +1,20 @@
+rm(list = ls())
+setwd("/home/creambbq/code/bikes/archivos")
+library(tidyverse)
+library(sf)
+library(ggthemes)
+load("cleanData.RData")
+bairesMap <- st_read("http://cdn.buenosaires.gob.ar/datosabiertos/datasets/barrios/barrios.geojson",
+                     stringsAsFactors = FALSE)
+bairesMap <- bairesMap %>% mutate(km2 = as.numeric(AREA)/1000000)
+stations <- ecoBiciData %>% group_by(nombre_estacion_origen,lat_estacion_origen,long_estacion_origen) %>% summarise() %>%  ungroup()
+
+ggplot() + 
+  geom_sf(data = bairesMap, color = "snow4") +
+  geom_point(data = stations, aes(x = long_estacion_origen, y = lat_estacion_origen),
+             color = "orange", size = 2, alpha = 0.6) +
+  labs(title = "Estaciones de EcoBici en los barrios de CABA", 
+       subtitle = "Año 2021") +
+  theme_map() +
+  theme(plot.title = element_text(size = 12, face = "bold"))
+
