b50977608db78d6752e430039fe70a19faa53861
CreamBBQ <jgmd1306@gmail.com>
2022-02-13 15:42:34 -0300

    Creo un nuevo dataset de estaciones con carácteristicas espaciales

--- test.R
+++ test.R
@@ -3,12 +3,12 @@ setwd("/home/creambbq/code/bikes/archivos")
 library(tidyverse)
 library(sf)
 library(ggthemes)
-load("cleanData.RData")
+load("dataClean.RData")
 bairesMap <- st_read("http://cdn.buenosaires.gob.ar/datosabiertos/datasets/barrios/barrios.geojson",
                      stringsAsFactors = FALSE)
 bairesMap <- bairesMap %>% mutate(km2 = as.numeric(AREA)/1000000)
 stations <- ecoBiciData %>% group_by(nombre_estacion_origen,lat_estacion_origen,long_estacion_origen) %>% summarise() %>%  ungroup()
-
+#Gráfico de densidad de estaciones por barrio. 
 ggplot() + 
   geom_sf(data = bairesMap, color = "snow4") +
   geom_point(data = stations, aes(x = long_estacion_origen, y = lat_estacion_origen),
@@ -18,5 +18,8 @@ ggplot() +
   theme_map() +
   theme(plot.title = element_text(size = 12, face = "bold"))
 
-
+#Gráfico de densidad de estaciones por comuna 
+#sum(is.na(stations))
+#class(bairesMap$COMUNA)
+spaceStations <- stations %>% st_as_sf(coords = c("long_estacion_origen", "lat_estacion_origen"), crs = 4326)
 
